9800c7ed423ea9043e08e0d24bb5f97072b45c2b
C. Allwardt <3979063+craig8@users.noreply.github.com>
2022-10-06 13:07:37 -0700

    Move ExternalRPCService and PubSubService to the router service This is the only location in the code base that they are needed and therefore should not be considered "top level" services.

--- src/volttron/services/routing/pubsub_service.py
+++ src/volttron/services/routing/pubsub_service.py
@@ -67,7 +67,7 @@ _ROUTE_ERRORS = {
 _log = logging.getLogger(__name__)
 
 
-class PubSubService(object):
+class PubSubService:
 
     def __init__(self, socket, protected_topics, routing_service, *args, **kwargs):
         self._logger = logging.getLogger(__name__)
