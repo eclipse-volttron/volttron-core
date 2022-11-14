ab18b58241fc2520176a84d45609ee3dffa248e8
C. Allwardt <3979063+craig8@users.noreply.github.com>
2022-11-14 15:58:06 -0800

    update identities.

--- src/volttron/client/known_identities.py
+++ src/volttron/client/known_identities.py
@@ -53,10 +53,10 @@ PLATFORM_HISTORIAN = "platform.historian"
 PLATFORM_MARKET_SERVICE = "platform.market"
 
 ROUTER = ""
-CONTROL = "volttron.platform.control"
+CONTROL = "platform.control"
 CONTROL_CONNECTION = "control.connection"
-PLATFORM_WEB = "volttron.platform.web"
-CONFIGURATION_STORE = "volttron.platform.config_store"
+PLATFORM_WEB = "platform.web"
+CONFIGURATION_STORE = "platform.config_store"
 KEY_DISCOVERY = "keydiscovery"
 PROXY_ROUTER = "zmq.proxy.router"
 
