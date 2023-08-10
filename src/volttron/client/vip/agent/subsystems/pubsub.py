bb02b1fcd739d4f3d6bb060f0a3d757bc98bcbd1
Craig <3979063+craig8@users.noreply.github.com>
2023-08-10 11:54:49 -0700

    Update pubsub.py
    Added documentation back

--- src/volttron/client/vip/agent/subsystems/pubsub.py
+++ src/volttron/client/vip/agent/subsystems/pubsub.py
@@ -186,6 +186,7 @@ class PubSub(SubsystemBase):
         def subscriptions():
             return defaultdict(set)
 
+        # format d[platform][bus][prefix] = set(callbacks)
         subscriptions_by_tag = defaultdict(platform_subscriptions)
         for platform, bus_subscriptions in self._my_tag_condition_callbacks.items():
             for bus, tag_conditions in bus_subscriptions.items():
