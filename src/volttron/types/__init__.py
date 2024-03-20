c233c98a58039d41147d364677e30066dffa410f
C. Allwardt <3979063+craig8@users.noreply.github.com>
2024-03-20 13:12:15 -0700

    Move service interface to own module.

--- src/volttron/types/__init__.py
+++ src/volttron/types/__init__.py
@@ -27,35 +27,9 @@ from typing import Any, Dict
 import gevent
 from gevent import Greenlet
 
-from volttron.client.vip.agent import Agent
+from volttron.types.agent_context import AgentContext
 from volttron.types.bases import Service
 from volttron.types.message import Message
 from volttron.types.server_config import ServerConfig
 
-
-class ServiceInterface(Agent):
-
-    def __init__(self, server_config: ServerConfig = None, **kwargs):
-        self._server_config = server_config
-        super().__init__(**kwargs)
-
-    @classmethod
-    def get_kwargs_defaults(cls) -> Dict[str, Any]:
-        """
-        Class method that allows the specific class to have the ability to specify
-        what service arguments are available as defaults.
-        """
-        return {}
-
-    def spawn_in_greenlet(self) -> Greenlet:
-        """
-        Start the execution of a volttron agent.
-        """
-        event = gevent.event.Event()
-        task = gevent.spawn(self.core.run, event)
-        event.wait()
-        del event
-        return task
-
-
-__all__: list[str] = ['ServiceInterface', 'Service', 'Message']
+__all__: list[str] = ['Service', 'Message', 'AgentContext']
