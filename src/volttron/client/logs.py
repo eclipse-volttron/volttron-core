7806ce5e153acd869e3e1809de11e03f83a2dad0
C. Allwardt <3979063+craig8@users.noreply.github.com>
2024-07-27 12:48:53 -0700

    Add new logs and logger files.

---
+++ src/volttron/client/logs.py
@@ -0,0 +1,89 @@
+import logging
+from logging import Logger
+
+
+def get_logger() -> Logger:
+    return logging.getLogger("volttron.client")
+
+
+def get_default_client_log_config(level=logging.DEBUG) -> dict:
+    return {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "simple": {
+                "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                "datefmt": "%Y-%m-%d %H:%M:%S"
+            }
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "level": level,
+                "formatter": "simple",
+                "stream": "ext://sys.stdout"
+            },
+            "file": {
+                "class": "logging.FileHandler",
+                "level": "INFO",
+                "formatter": "simple",
+                "filename": "myapp.log",
+                "mode": "a"
+            }
+        },
+        "loggers": {
+            "volttron.client": {
+                "level": level
+            }
+        },
+        "root": {
+            "level": level,
+            "handlers": ["console"]
+        }
+    }
+
+
+class AgentFormatter(logging.Formatter):
+
+    def __init__(self, fmt=None, datefmt=None):
+        if fmt is None:
+            fmt = "%(asctime)s %(composite_name)s %(levelname)s: %(message)s"
+        super(AgentFormatter, self).__init__(fmt=fmt, datefmt=datefmt)
+
+    def composite_name(self, record):
+        if record.name == "agents.log":
+            cname = "(%(processName)s %(process)d) %(remote_name)s"
+        elif record.name.startswith("agents.std"):
+            cname = "(%(processName)s %(process)d) <{}>".format(record.name.split(".", 2)[1])
+        else:
+            cname = "() %(name)s"
+        return cname % record.__dict__
+
+    def format(self, record):
+        if "composite_name" not in record.__dict__:
+            record.__dict__["composite_name"] = self.composite_name(record)
+        return super(AgentFormatter, self).format(record)
+
+
+def setup_logging(level=logging.DEBUG, console=False):
+    root = logging.getLogger()
+    if not root.handlers:
+        handler = logging.StreamHandler()
+
+        if isapipe(sys.stderr) and "_LAUNCHED_BY_PLATFORM" in os.environ:
+            handler.setFormatter(JsonFormatter())
+        elif console:
+            # Below format is more readable for console
+            handler.setFormatter(logging.Formatter("%(levelname)s: %(message)s"))
+        else:
+            fmt = "%(asctime)s %(name)s %(levelname)s: %(message)s"
+            handler.setFormatter(logging.Formatter(fmt))
+        if level != logging.DEBUG:
+            # import it here so that when urllib3 imports the requests package, ssl would already got
+            # monkey patched by gevent.
+            # and this warning is needed only when log level is not debug
+            from urllib3.exceptions import InsecureRequestWarning
+
+            warnings.filterwarnings("ignore", category=InsecureRequestWarning)
+        root.addHandler(handler)
+    root.setLevel(level)
