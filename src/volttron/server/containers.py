44524ce72137c97df89e983c696c72b6ff1fe8ef
C. Allwardt <3979063+craig8@users.noreply.github.com>
2025-06-18 22:58:46 -0700

    Add print_resolved method to Container for better debugging output

--- src/volttron/server/containers.py
+++ src/volttron/server/containers.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import inspect
 import logging
+import sys
 import typing
 from re import match
 from typing import Optional, TypeVar, Union, get_type_hints
@@ -74,6 +75,25 @@ class Container:
         Container.containers[name] = self
         self._resolvable: dict[T, S] = {}
         self._resolvable_lists: dict[T, Container.ResolvableList] = {}
+
+    def print_resolved(self, stream=sys.stdout):
+        stream.write(f'{"-" * 80}\n')
+        if self._resolvable_lists:
+            stream.write("Lists\n")
+            for r, v in self._resolvable_lists.items():
+                stream.write(f"\t{r} -> {v}\n")
+        else:
+            stream.write("No Resolvable Lists Found")
+
+        if self._resolvable:
+            stream.write("Singletons")
+            for r, v in self._resolvable.items():
+                stream.write(f"\t{r}-> {v}\n")
+            
+        else:
+            stream.write("No resolved items found")
+        stream.write(f'{"-" * 80}\n')
+
 
     @staticmethod
     def destroy_all():
@@ -259,8 +279,6 @@ class Container:
             self._resolved = None
             self._kwargs = kwargs
             self._container: Container = container
-
-            # self._kwargs = {k: v for k, v in kwargs.items()}
 
         @property
         def type(self) -> S:
